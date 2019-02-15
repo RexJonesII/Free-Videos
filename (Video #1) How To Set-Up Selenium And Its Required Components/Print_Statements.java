d38397c09bedf43c5da9f960cfc8e932220619cd
Rex Jones II <Rex.Jones@Test4Success.org>
2019-02-15 05:05:03 -0600

    Add files via upload

---
+++ (Video #1) How To Set-Up Selenium And Its Required Components/Print_Statements.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class Print_Statements 
+{
+	@Test
+	public void printHelloWorld ()
+	{
+		System.out.println("Hello World!!!");
+	}
+	
+	@Test
+	public void printWelcomeMessage ()
+	{
+		System.out.println("Welcome To Selenium 4 Beginners");
+	}
+}
