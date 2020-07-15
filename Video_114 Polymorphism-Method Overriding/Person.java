8f7b13cd19282053e33bd6c39475d532b753c9eb
Rex Jones II <Rex.Jones@Test4Success.org>
2020-07-15 18:30:14 -0500

    Add files via upload

---
+++ Video_114 Polymorphism-Method Overriding/Person.java
@@ -0,0 +1,19 @@
+package com.selenium4beginners.java.oop.polymorphism.override;
+
+public class Person {
+	private String name;
+	private String dateOfBirth;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+}
