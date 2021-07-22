febfcdf2b8fbda91b1d6361ed953ef6ec29564b1
Rex Jones II <Rex.Jones@Test4Success.org>
2021-07-21 19:36:09 -0500

    Add files via upload

---
+++ Python Programming/Video 256_Import Modules_Functions/module_converters.py
@@ -0,0 +1,10 @@
+
+print('Import Converters Module')
+
+tutorial = 'This Is A Python Import Tutorial'
+
+def yards_to_feet(yds):
+  return yds * 3
+
+def feet_to_yards(ft):
+  return ft * 0.333
---
+++ Python Programming/Video 256_Import Modules_Functions/module_main.py
@@ -0,0 +1,11 @@
+from module_converters import feet_to_yards, yards_to_feet
+import module_converters as mc
+import module_converters
+
+print(module_converters.tutorial)
+print(mc.yards_to_feet(10), 'feet')
+print(feet_to_yards(10), 'yards')
+
+
+
+
