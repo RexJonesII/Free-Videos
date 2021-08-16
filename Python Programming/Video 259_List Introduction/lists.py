05be0f0dd5c66a9cc4f07064657bb180a4896bea
Rex Jones II <Rex.Jones@Test4Success.org>
2021-08-16 05:17:25 -0500

    Add files via upload

---
+++ Python Programming/Video 259_List Introduction/lists.py
@@ -0,0 +1,14 @@
+numbers = [5, 8, 2, 1, 7, 8]
+letters = ['A', 'a', 'X', 'b', 'Jane', 'John', 'James']
+booleans = [[0, 'False'], [1,'True'], [-1, 'Error']]
+#            0,0    0,1    1,0  1,1     2,0     2,1
+
+print(numbers[1.0])
+
+
+
+
+
+
+
+
